09e2a459b98a0f3be8b33d1fedc1f5f94ed47310
Edward <edwardshei@gmail.com>
2021-05-27 12:33:06 -0700

    forgot to save slide component

--- client/src/Slide.jsx
+++ client/src/Slide.jsx
@@ -9,6 +9,22 @@ export default function Slide(props) {
 			style={{
 				backgroundImage: `url(${media.desktop})`,
 			}}
-		 />
+		 >
+         <div className = {"slideContent"}>
+                <h1 className = 'title'>{title.toUpperCase()}</h1>
+                <p className = 'heading'>{heading}</p>
+                {cta.map((element, index) => {
+                    return (
+                        <a className = "buttonLink"
+                            href = {element.url}
+                            target = "_blank"
+                            key = {index}
+                        >
+                            {element.label.toUpperCase()}
+                        </a>
+                    )
+                })}
+         </div>
+         </div>
     )
 }
